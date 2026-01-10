9e91a6fc38c245fff9b020c528968f8c131d4e6c
Qanz4Ever <qyumassa@gmail.com>
2026-01-10 19:54:09 +0700

    Enhance order functions and star animation

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 //File Created By Mfsavana
 //Don't Steal Anything
+
 for (let i = 0; i < 200; i++) {
   const star = document.createElement("div");
   star.className = "star";
-
   const size = Math.random() * 2 + 1;
   star.style.width = size + "px";
   star.style.height = size + "px";
-
   star.style.left = Math.random() * window.innerWidth + "px";
   star.style.top = Math.random() * -window.innerHeight + "px";
-
-  const duration = Math.random() * 10 + 5; // 5–15 detik
+  const duration = Math.random() * 10 + 5;
   star.style.animationDuration = `2s, ${duration}s`;
-
   document.body.appendChild(star);
 }
 
@@ -31,7 +28,6 @@ let toastTimer = null;
 function showToast(message) {
   toast.textContent = message;
   toast.classList.add("show");
-
   clearTimeout(toastTimer);
   toastTimer = setTimeout(() => {
     toast.classList.remove("show");
@@ -49,7 +45,19 @@ function copyText(text) {
 }
 
 function orderPanel(panel, harga) {
-  const text = `Halo Wanz, saya ingin membeli panel ${panel} dengan harga ${harga}`;
+  const text = `Halo Wanz, saya ingin membeli Panel Pterodactyl dengan spesifikasi ${panel} dengan harga ${harga}`;
+  const url = `https://wa.me/6281460365472?text=${encodeURIComponent(text)}`;
+  window.open(url, "_blank");
+}
+
+function orderVPS(type, harga) {
+  const text = `Halo Wanz, saya ingin membeli Vps ${type} dengan harga ${harga}`;
+  const url = `https://wa.me/6281460365472?text=${encodeURIComponent(text)}`;
+  window.open(url, "_blank");
+}
+
+function orderMember(type, harga) {
+  const text = `Halo Wanz, saya ingin membeli Membership Panel yang ${type} dengan harga ${harga}`;
   const url = `https://wa.me/6281460365472?text=${encodeURIComponent(text)}`;
   window.open(url, "_blank");
 }
